2739a36a089266327fb4c91037ada13e18284144
qovery <github@qovery.com>
2022-02-19 10:37:55 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/29c1a8663e4d816631a8d8f1a9347333bd5dea24 from refs/heads/main

--- configuration.go
+++ configuration.go
@@ -102,7 +102,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "OpenAPI-Generator/1.0.0/go",
+		UserAgent:     "OpenAPI-Generator/1.0.1/go",
 		Debug:         false,
 		Servers: ServerConfigurations{
 			{
