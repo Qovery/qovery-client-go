8acfd10ae705e81e2be7e584495ea368e7672060
qovery <github@qovery.com>
2023-04-12 11:01:56 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/3c87f8a1e8fc723892f76416615669d0c5ac7305 from refs/heads/main

--- docs/EnvironmentLogsDetailsStage.md
+++ docs/EnvironmentLogsDetailsStage.md
@@ -5,6 +5,8 @@
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **Step** | Pointer to **string** |  | [optional] 
+**Id** | Pointer to **NullableString** |  | [optional] 
+**Name** | Pointer to **NullableString** |  | [optional] 
 
 ## Methods
 
@@ -50,6 +52,76 @@ SetStep sets Step field to given value.
 
 HasStep returns a boolean if a field has been set.
 
+### GetId
+
+`func (o *EnvironmentLogsDetailsStage) GetId() string`
+
+GetId returns the Id field if non-nil, zero value otherwise.
+
+### GetIdOk
+
+`func (o *EnvironmentLogsDetailsStage) GetIdOk() (*string, bool)`
+
+GetIdOk returns a tuple with the Id field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetId
+
+`func (o *EnvironmentLogsDetailsStage) SetId(v string)`
+
+SetId sets Id field to given value.
+
+### HasId
+
+`func (o *EnvironmentLogsDetailsStage) HasId() bool`
+
+HasId returns a boolean if a field has been set.
+
+### SetIdNil
+
+`func (o *EnvironmentLogsDetailsStage) SetIdNil(b bool)`
+
+ SetIdNil sets the value for Id to be an explicit nil
+
+### UnsetId
+`func (o *EnvironmentLogsDetailsStage) UnsetId()`
+
+UnsetId ensures that no value is present for Id, not even an explicit nil
+### GetName
+
+`func (o *EnvironmentLogsDetailsStage) GetName() string`
+
+GetName returns the Name field if non-nil, zero value otherwise.
+
+### GetNameOk
+
+`func (o *EnvironmentLogsDetailsStage) GetNameOk() (*string, bool)`
+
+GetNameOk returns a tuple with the Name field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetName
+
+`func (o *EnvironmentLogsDetailsStage) SetName(v string)`
+
+SetName sets Name field to given value.
+
+### HasName
+
+`func (o *EnvironmentLogsDetailsStage) HasName() bool`
+
+HasName returns a boolean if a field has been set.
+
+### SetNameNil
+
+`func (o *EnvironmentLogsDetailsStage) SetNameNil(b bool)`
+
+ SetNameNil sets the value for Name to be an explicit nil
+
+### UnsetName
+`func (o *EnvironmentLogsDetailsStage) UnsetName()`
+
+UnsetName ensures that no value is present for Name, not even an explicit nil
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- model_environment_logs_details_stage.go
+++ model_environment_logs_details_stage.go
@@ -17,7 +17,9 @@ import (
 
 // EnvironmentLogsDetailsStage struct for EnvironmentLogsDetailsStage
 type EnvironmentLogsDetailsStage struct {
-	Step *string `json:"step,omitempty"`
+	Step *string        `json:"step,omitempty"`
+	Id   NullableString `json:"id,omitempty"`
+	Name NullableString `json:"name,omitempty"`
 }
 
 // NewEnvironmentLogsDetailsStage instantiates a new EnvironmentLogsDetailsStage object
@@ -69,10 +71,102 @@ func (o *EnvironmentLogsDetailsStage) SetStep(v string) {
 	o.Step = &v
 }
 
+// GetId returns the Id field value if set, zero value otherwise (both if not set or set to explicit null).
+func (o *EnvironmentLogsDetailsStage) GetId() string {
+	if o == nil || o.Id.Get() == nil {
+		var ret string
+		return ret
+	}
+	return *o.Id.Get()
+}
+
+// GetIdOk returns a tuple with the Id field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
+func (o *EnvironmentLogsDetailsStage) GetIdOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return o.Id.Get(), o.Id.IsSet()
+}
+
+// HasId returns a boolean if a field has been set.
+func (o *EnvironmentLogsDetailsStage) HasId() bool {
+	if o != nil && o.Id.IsSet() {
+		return true
+	}
+
+	return false
+}
+
+// SetId gets a reference to the given NullableString and assigns it to the Id field.
+func (o *EnvironmentLogsDetailsStage) SetId(v string) {
+	o.Id.Set(&v)
+}
+
+// SetIdNil sets the value for Id to be an explicit nil
+func (o *EnvironmentLogsDetailsStage) SetIdNil() {
+	o.Id.Set(nil)
+}
+
+// UnsetId ensures that no value is present for Id, not even an explicit nil
+func (o *EnvironmentLogsDetailsStage) UnsetId() {
+	o.Id.Unset()
+}
+
+// GetName returns the Name field value if set, zero value otherwise (both if not set or set to explicit null).
+func (o *EnvironmentLogsDetailsStage) GetName() string {
+	if o == nil || o.Name.Get() == nil {
+		var ret string
+		return ret
+	}
+	return *o.Name.Get()
+}
+
+// GetNameOk returns a tuple with the Name field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
+func (o *EnvironmentLogsDetailsStage) GetNameOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return o.Name.Get(), o.Name.IsSet()
+}
+
+// HasName returns a boolean if a field has been set.
+func (o *EnvironmentLogsDetailsStage) HasName() bool {
+	if o != nil && o.Name.IsSet() {
+		return true
+	}
+
+	return false
+}
+
+// SetName gets a reference to the given NullableString and assigns it to the Name field.
+func (o *EnvironmentLogsDetailsStage) SetName(v string) {
+	o.Name.Set(&v)
+}
+
+// SetNameNil sets the value for Name to be an explicit nil
+func (o *EnvironmentLogsDetailsStage) SetNameNil() {
+	o.Name.Set(nil)
+}
+
+// UnsetName ensures that no value is present for Name, not even an explicit nil
+func (o *EnvironmentLogsDetailsStage) UnsetName() {
+	o.Name.Unset()
+}
+
 func (o EnvironmentLogsDetailsStage) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
 	if o.Step != nil {
 		toSerialize["step"] = o.Step
+	}
+	if o.Id.IsSet() {
+		toSerialize["id"] = o.Id.Get()
+	}
+	if o.Name.IsSet() {
+		toSerialize["name"] = o.Name.Get()
 	}
 	return json.Marshal(toSerialize)
 }
