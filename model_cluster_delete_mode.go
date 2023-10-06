bfcb9c3ce26182699e62dca455f0a14695cade44
qovery <github@qovery.com>
2023-10-06 09:58:08 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/9037730fb368454270ce061bd53954237303cd73 from refs/heads/main

--- model_cluster_delete_mode.go
+++ model_cluster_delete_mode.go
@@ -16,7 +16,7 @@ import (
 	"fmt"
 )
 
-// ClusterDeleteMode Indicates the mode to apply on cluster deletion   **\"hard delete\"** means that we delete directly from our database, this is different from a **\"trigger delete\"** that cleans the resource - `DEFAULT`: this is the normal way, trigger delete the cluster only if no environment linked to this cluster remains - `DELETE_CLUSTER_AND_QOVERY_CONFIG`: hard delete environments linked to this cluster then trigger delete the cluster - `DELETE_QOVERY_CONFIG`: hard delete environments linked to this cluster then hard delete the cluster
+// ClusterDeleteMode Indicates the mode to apply on cluster deletion   **\"hard delete\"** means that we delete directly from our database, this is different from a **\"trigger delete\"** that cleans the resource - `DEFAULT`: this is the normal way, trigger delete the cluster only if no environment linked to this cluster remains - `DELETE_CLUSTER_AND_QOVERY_CONFIG`: hard delete environments linked to this cluster then trigger delete the cluster - `DELETE_QOVERY_CONFIG`: ⚠️ ⚠️ ⚠️ hard delete environments linked to this cluster then hard delete the cluster - whole cluster ressources **are not deleted** on our side and must be deleted on your side
 type ClusterDeleteMode string
 
 // List of ClusterDeleteMode
